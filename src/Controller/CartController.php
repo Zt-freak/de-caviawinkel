b8dd10584e7ddfbef0f31b5ee63f789a36ce3b15
Zt-freak <bnkho98@hotmail.com>
2019-09-10 11:19:15 +0200

    added pseudo-checkout

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+use App\Form\CheckoutForm;
+
 /**
  * @Route("/cart")
  */
@@ -39,6 +41,53 @@ class CartController extends AbstractController
 
         return $this->render('cart/index.html.twig', [
             'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/checkout", name="cart_checkout", methods={"GET", "POST"})
+     */
+    public function checkout(Request $request, \Swift_Mailer $mailer, ProductRepository $productRepository): Response
+    {
+        $cart = $this->session->get('cart');
+        $products = [];
+
+        foreach ($cart as &$value) {
+            $product = $productRepository->findById($value[0])[0];
+            array_push($products, ["id"=>$product->getId(), "name"=>$product->getName(), "price"=>$product->getPrice()]);
+        }
+
+        $form = $this->createForm(CheckoutForm::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $checkout = $form->getData();
+
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('1037390@mborijnland.nl')
+                ->setTo($checkout["e-mail"])
+                ->setBody(
+                    $this->renderView(
+                        'cart/checkout_success.html.twig', [
+                            'products' => $products,
+                            'checkoutData' => $checkout
+                        ]
+                    ),
+                    'text/html'
+                )
+            ;
+
+            $mailer->send($message);
+    
+            return $this->render('cart/checkout_success.html.twig', [
+                'products' => $products,
+                'checkoutData' => $checkout
+            ]);
+        }
+
+        return $this->render('cart/checkout.html.twig', [
+            'products' => $products,
+            'form' => $form->createView(),
         ]);
     }
 }
